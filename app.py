0ad7ea01d85b0b102fa7762c3949719e1fde335a
AuenKr <auenkumar64@gmail.com>
2023-07-09 21:51:41 +0530

    Removed Unnecessary Comments

--- app.py
+++ app.py
@@ -1,30 +1,16 @@
-import streamlit as st
-# To load api keys from .env file
 from dotenv import load_dotenv
-# To get pdf text
+import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-
-# To convert & store chunks into vectorForm
-# From OpenAI
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
-# From HuggingFace
-# from langchain.embeddings import HuggingFaceInstructEmbeddings
-
-# Buffer Memory
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-
-# Language Model
 from langchain.chat_models import ChatOpenAI
-
-# HTML templete
 from htmlTemplelates import css, bot_template, user_template
 
+
 # To read pdf
-
-
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
@@ -36,34 +22,16 @@ def get_pdf_text(pdf_docs):
 
 # Define Text Spliter Properties
 def get_text_chunks(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-    )
-
-    # Chunks in form of list
+    text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len )
     chunks = text_splitter.split_text(text)
     return chunks
 
 
-# convert list into embedding
-
-# OpenAi
+# convert list into embedding (OpenAi ada model)
 def get_vector_storeOpenAI(text_chunks):
     embeddings = OpenAIEmbeddings()
     vectorStore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorStore
-
-
-# HuggingFace(Under Devlopment)
-# '''
-# def get_vector_storeHuggingFace(text_chunks):
-#     embeddings = HuggingFaceInstructEmbeddings(model_name ="hkunlp/instructor-xl")
-#     vectorStore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-#     return vectorStore
-# '''
 
 
 # Conversation Chain
@@ -71,34 +39,30 @@ def get_conversation_chain(vectorStore):
     llm = ChatOpenAI()
     memory = ConversationBufferMemory(
         memory_key="chat_history", return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorStore.as_retriever(),
-        memory=memory
-    )
+    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vectorStore.as_retriever(), memory=memory)
     return conversation_chain
 
 
+# Chat Session History
 def handle_userinput(user_question):
     response = st.session_state.conversation({"question": user_question})
     st.session_state.chat_history = response["chat_history"]
 
     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
-            st.write(user_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
         else:
-            st.write(bot_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
 
 
+# Main Page
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with Multiple PDFs", page_icon=":books:")
 
     st.write(css, unsafe_allow_html=True)
 
-    # Session state
+    # Session state variable
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
     if "chat_history" not in st.session_state:
@@ -112,29 +76,14 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", type="pdf", accept_multiple_files=True)
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", type="pdf", accept_multiple_files=True)
 
         if st.button("Process"):
             with st.spinner("Processing"):
-                # Get pdf text
                 raw_text = get_pdf_text(pdf_docs)
-
-                # Get text chunks
                 text_chunk = get_text_chunks(raw_text)
-
-                # create vector store
-                # 1. Using OpenAi embedding model
                 vectorStore = get_vector_storeOpenAI(text_chunk)
-
-                # 2. HuggingFace Instructor
-                # vectorStore = get_vector_storeHuggingFace(text_chunk)
-
-                # Create conversation chain
-                # session_state - if some state is change then st will reload the entire code, but we want to retrive conversation so we use this; This also make it similar to Global variable
-
                 st.session_state.conversation = get_conversation_chain(vectorStore)
-        
 
 
 if __name__ == '__main__':
